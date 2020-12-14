d6535cd4f4915c7e13b33e55bf63dc97d6aa5d4f
Maniteja <sreemanitejateja@gmail.com>
2020-12-14 18:43:16 +0530

    divided homepage component into multiple components and styles

--- toy-shop-app/src/components/menu-item/menu-item.js
+++ toy-shop-app/src/components/menu-item/menu-item.js
@@ -3,9 +3,11 @@ import "./menu-item.css"
 
 const MenuItem = (props) =>{
     return(
-        <div style={{
+        <div  className={`${props.size} menu-item`}>
+        <div className = "background-image" 
+        style={{
             backgroundImage: `url(${props.imageURL})`
-        }} className={`${props.size} menu-item`}>
+        }}/>
             <div class="content">
                 <h1 className="title">{props.title}</h1>
                 <span clasName="subtitle">Buy Now</span>
