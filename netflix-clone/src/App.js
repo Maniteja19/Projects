979d9d90f5d851a9ef9309a3f63189a4be8bb7d8
Maniteja <sreemanitejateja@gmail.com>
2020-12-23 12:20:13 +0530

    added netflix-clone basic structure

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home  from './pages/home'
+
+function App() {
+  return (
+    <Router>
+        <Switch>
+            <Route path="/signin">
+                <p>I will be the sign in page</p>
+            </Route>
+            <Route path="/signup">
+                <p>I will be the sign up page</p>
+            </Route>
+            <Route path="/browse">
+                <p>I will be the browse page</p>
+            </Route>
+            <Route path="/">
+                <Home />
+            </Route>
+        </Switch>
+    </Router>
+  );
+}
+
+export default App
