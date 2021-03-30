fd86a5dcdfc8b69d5d9d6a8a93949b1ae3fe4e1d
Maniteja <sreemanitejateja@gmail.com>
2021-03-30 11:40:15 +0530

    add mobile layout

--- nikhitha_portfolio_project/javascript/index.js
+++ nikhitha_portfolio_project/javascript/index.js
@@ -3,11 +3,17 @@ let h1 = document.querySelector('h1')
 let open = document.querySelector('.open-btn')
 let close = document.querySelector('.close-btn')
 let links = document.querySelector('.links')
+let link = document.querySelector('.link')
+
 
 open.addEventListener('click', function () {
     links.style.display = "flex"
 })
 
 close.addEventListener('click', function () {
+    links.style.display = "none"
+})
+
+link.addEventListener('click', function () {
     links.style.display = "none"
 })
