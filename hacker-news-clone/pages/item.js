8e1890846d3e331198d862f8f3da842f0d89aac4
Maniteja <sreemanitejateja@gmail.com>
2021-06-11 10:24:03 +0530

    format comments page

--- hacker-news-clone/pages/item.js
+++ hacker-news-clone/pages/item.js
@@ -1,5 +1,6 @@
 import Story from '../components/Story.js';
 import view from '../utils/view.js';
+import Comment from '../components/Comment.js';
 
 export default async function Item() {
     let story = null;
@@ -23,7 +24,7 @@ export default async function Item() {
     ${Story(story)}
   </div>
   <hr/>
-  ${hasComments ? story.comments.map(comment => JSON.stringify(comment)).join('') : 'No comments'}
+  ${hasComments ? story.comments.map(comment => Comment(comment)).join('') : 'No comments'}
   `
 }
 
